9a8a8ce1d8a953afbd8223c7fbd7e9baddbf406c
nokia6102 <nokia6102@gmail.com>
2014-12-01 19:49:46 +0800

    Initial Commit

---
+++ pureC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pureC
+//
+//  Created by Leon Cheng on 2014/12/1.
+//  Copyright (c) 2014年 Leon Cheng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
